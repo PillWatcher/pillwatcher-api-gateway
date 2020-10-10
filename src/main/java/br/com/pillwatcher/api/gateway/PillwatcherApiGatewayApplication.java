63026be2948739fcfa5ac83139ed4626d76666da
Amanda Pires <amandoca_779@hotmail.com>
2020-10-10 16:47:29 -0300

    :sparkles: Adding inital structure for api gateway

---
+++ src/main/java/br/com/pillwatcher/api/gateway/PillwatcherApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.pillwatcher.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillwatcherApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillwatcherApiGatewayApplication.class, args);
+	}
+
+}
